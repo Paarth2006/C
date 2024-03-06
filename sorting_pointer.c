e867f4ed5d1bda77615c144a0053d361be8162ab
Paarth <singh.paarth10@gmail.com>
2024-03-06 19:47:48 +0530

    pointer

---
+++ sorting_pointer.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+  int n,arr[100],i,j,*p,t=0;
+  p=arr;
+  printf("Enter the Size of the array:");
+  scanf("%d",&n);
+  printf("Enter the %d elements of the array:",n);
+  for (i=0;i<n;i++)
+  {
+    scanf("%d",(p+i));
+  }
+  for (i=0;i<n;i++)
+  {
+    for (j=i+1;j<n;j++)
+    {
+     if (*(p+i)>*(p+j))
+     {
+      	t=*(p+i);
+      	*(p+i)=*(p+j);
+      	*(p+j)=t;
+     }
+    }
+  }
+  printf("The Sorted Array is :");
+  for (i=0;i<n;i++)
+  {
+   printf("%d ",*(p+i));
+  }
+  return 0;
+}
