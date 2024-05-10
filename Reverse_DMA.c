66a6900b16cc43f9273d35193874cee75a9f6ebf
Paarth <singh.paarth10@gmail.com>
2024-05-11 00:20:11 +0530

    DMA

---
+++ Reverse_DMA.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,j,n,*p,t;
+    printf("\t\t*****INPUT*****\n");
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+    p = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the %d element of the array:",i+1);
+        scanf("%d", (p+i));
+    }
+    int index;
+    printf("Enter the postion to reverse from:");
+    scanf("%d",&index);
+    for (i=index-1,j=n-1;i<j;i++,j--)
+    {
+        t = *(p+i);
+        *(p+i) = *(p+j);
+        *(p+j) = t;
+    }
+    printf("\t\t*****OUTPUT*****\n");
+    printf("Reversed Array is :");
+    for (i=0;i<n;i++)
+    {
+        printf("%d ",*(p+i));
+
+    }
+    free(p);
+    return 0;
+}
